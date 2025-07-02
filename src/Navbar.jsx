9117f866a89743258d0f53321efb72588720fff3
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-02 09:43:23 +0530

    chore: remove redunadant imports and structure code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,18 +2,9 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import UserMenu from './UserMenu'
 import "./Navbar.css"
 
 // icons
@@ -43,27 +34,18 @@ function DrawerAppBar(props) {
         console.log("settings clicked");
     }
 
-    const container = window !== undefined ? () => window().document.body : undefined;
+    // const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
         <Box sx={{ display: 'flex' }}>
             <AppBar component="nav">
                 <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { sm: 'none' } }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
                     <Typography
                         variant="h6"
                         component="div"
                         sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
                     >
-
+                    Zenith
                     </Typography>
                     <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                         <button onClick={handelMusic} className='control-btn'>
@@ -93,6 +75,18 @@ DrawerAppBar.propTypes = {
 export default DrawerAppBar;
 
 
+// --------------------------------
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Divider from '@mui/material/Divider';
+// import Drawer from '@mui/material/Drawer';
+// import IconButton from '@mui/material/IconButton';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemText from '@mui/material/ListItemText';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import Button from '@mui/material/Button';
+
 // For mobile
     //  For mobile icon 
     // const handleDrawerToggle = () => {
@@ -113,6 +107,16 @@ export default DrawerAppBar;
     //         </List>
     //     </Box>
     // );
+    // ------------------------
+    // <IconButton
+    //                     color="inherit"
+    //                     aria-label="open drawer"
+    //                     edge="start"
+    //                     onClick={handleDrawerToggle}
+    //                     sx={{ mr: 2, display: { sm: 'none' } }}
+    //                 >
+    //                 <MenuIcon />
+    //                 </IconButton>
 
     // --------------------------
     //  <nav>
