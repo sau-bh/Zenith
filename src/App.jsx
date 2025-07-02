3fee379518d9f6012d4d2be8bfbfefe49a25bfde
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-02 08:39:59 +0530

    feat: implemet navbar and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Timer from "./Timer"
 import PomodoroTimer from "./PomodoroTimer"
+import Navbar from "./Navbar"
 
 function App() {
 
   return (
     <>
+      <Navbar></Navbar>
       {/* <Timer></Timer> */}
       <PomodoroTimer></PomodoroTimer>
     </>
