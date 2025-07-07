28b7489325c1c0c72bf37d63a07ea2fe8a1e4340
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-07 07:57:38 +0530

    feat: implement FocusTimer with new props

--- src/FocusTimer.jsx
+++ src/FocusTimer.jsx
@@ -2,27 +2,17 @@ import { useEffect, useRef, useState } from "react"
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-export default function FocusTimer({ customTime, isPomodoro, setIsPomodoro }) {
-
-    const convertToMilliseconds = (time) => {
-        return ((time.hrs * 60 * 60) + (time.min * 60) + time.sec) * 1000;
-    };
+export default function FocusTimer({ duration, onComplete }) {
 
     const completeSound = useRef(new Audio("level-win-6416.mp3"));
-
-    const [defaultTime, setDefaultTime] = useState(convertToMilliseconds(customTime));
-    const [remainingTime, setRemainingTime] = useState(defaultTime);
+    const [remainingTime, setRemainingTime] = useState(duration);
     const [isRunning, setIsRunning] = useState(false);
 
-    const hasPlayedMusic = useRef(false);
+    const intervalRef = useRef(null);
 
     useEffect(() => {
-        const newTime = convertToMilliseconds(customTime);
-        setDefaultTime(newTime);
-        setRemainingTime(newTime);
-        setIsRunning(false);
-        hasPlayedMusic.current = false;
-    }, [customTime]);
+        setRemainingTime(duration);
+    }, [duration]);
 
 
 
@@ -32,63 +22,41 @@ export default function FocusTimer({ customTime, isPomodoro, setIsPomodoro }) {
         }
 
         const interval = setInterval(() => {
-            if (isPomodoro) {
-                setRemainingTime(
-                    prevTime => {
-                        if (prevTime < 1000) {
-                            clearInterval(interval);
-                            handleSessionComplete();
-                            return 0;
-                        }
-                        return prevTime - 1000;
-                    })
-            }
+            setRemainingTime(
+                prevTime => {
+                    if (prevTime < 1000) {
+                        clearInterval(intervalRef.current);
+                        completeSound.current.play();
+                        onComplete();
+                        return 0;
+                    }
+                    return prevTime - 1000;
+                });
         }, 1000);
 
-        return () => clearInterval(interval);
+        return () => clearInterval(intervalRef.current);
 
-    }, [isRunning, isPomodoro]);
-
-    // Handel complete for timer toggle
-    const handleSessionComplete = () => {
-        completeSound.current.play();
-        hasPlayedMusic.current = false;
-
-        setIsPomodoro((prev) => {
-            const next = !prev;
-
-            if (next) {
-                setRemainingTime(defaultTime);
-            } else {
-                setRemainingBreakTime(defaultBreakTime);
-            }
-            return next;
-        })
-        setIsRunning(true);
-    };
-
+    }, [isRunning, onComplete]);
 
     const startTimer = () => {
         setIsRunning(true);
-        hasPlayedMusic.current = true;
     }
 
     const stopTimer = () => {
-        clearInterval(intervalIdRef.current);
+        clearInterval(intervalRef.current);
         setIsRunning(false);
     }
 
     const resetTimer = () => {
+        clearInterval(intervalRef.current);
         setIsRunning(false);
-        setRemainingTime(defaultTime);
-        setIsPomodoro(true);
-        hasPlayedMusic.current = false;
+        setRemainingTime(duration);
     };
 
     // Displaying time on clock
     const displayFormatedTime = () => {
 
-        const time = isPomodoro ? remainingTime : remainingBreakTime;
+        const time = remainingTime;
 
         let hrs = Math.floor(time / (1000 * 60 * 60));
         let min = Math.floor((time / (1000 * 60)) % 60);
@@ -105,17 +73,16 @@ export default function FocusTimer({ customTime, isPomodoro, setIsPomodoro }) {
         }
     }
 
-    const totalTime = isPomodoro ? defaultTime : defaultBreakTime;
-    const timeLeft = isPomodoro ? remainingTime : remainingBreakTime;
+    const totalTime = duration;
+    const timeLeft = remainingTime;
     const percentageTime = ((totalTime - timeLeft) / totalTime) * 100;
 
 
     return (
         <>
             <div className="pomodoro-container">
-                <h2>{isPomodoro ? "Focus" : "Break Time"}</h2>
                 <CircularProgressbar
-                    value={percentageTime}
+                    value= {percentageTime}
                     text={displayFormatedTime()}
                     styles={buildStyles({
                         pathColor: "rgb(60, 62, 186)",
