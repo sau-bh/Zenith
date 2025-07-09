fd4f3241453c07975e431f267e0a2d3cb6458814
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-09 08:44:49 +0530

    feat: implement slider setting menu

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Themes from "./Themes.jsx"
+import CloseIcon from '@mui/icons-material/Close';
+import SaveIcon from '@mui/icons-material/Save';
+import SettingsIcon from '@mui/icons-material/Settings';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
 import { ListItem, ListItemIcon } from '@mui/material';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import CloseIcon from '@mui/icons-material/Close';
-import SettingsIcon from '@mui/icons-material/Settings';
-import Slide from '@mui/material/Slide';
+import CustomSound from "./CustomSound.jsx"
+import SetBGMVolume from './SetBGMVolume.jsx';
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-});
-
-export default function Settings() {
+export default function MaxWidthDialog() {
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
@@ -31,52 +29,34 @@ export default function Settings() {
 
     return (
         <React.Fragment>
+
             <ListItem disablePadding>
-                <ListItemButton onClick={handleClickOpen} style={{color:"black"}}>
-                    <ListItemIcon>
-                        <SettingsIcon/>
+                <ListItemButton onClick={handleClickOpen}>
+                    <ListItemIcon id='userIcon'>
+                        <SettingsIcon />
                     </ListItemIcon>
-                    <ListItemText primary="Settings"/>
+                    <ListItemText primary="Settings" />
                 </ListItemButton>
             </ListItem>
             <Dialog
-                fullScreen
+                fullWidth={true}
+                maxWidth={'sm'}
                 open={open}
                 onClose={handleClose}
-                slots={{
-                    transition: Transition,
-                }}
             >
-                <AppBar sx={{ position: 'relative' }}>
-                    <Toolbar>
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            onClick={handleClose}
-                            aria-label="close"
-                        >
-                            <CloseIcon />
-                        </IconButton>
-                        <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                            Sound
-                        </Typography>
-                        <Button autoFocus color="inherit" onClick={handleClose}>
-                            save
-                        </Button>
-                    </Toolbar>
-                </AppBar>
-                <List>
-                    <ListItemButton>
-                        <ListItemText primary="Phone ringtone" secondary="Titania" />
-                    </ListItemButton>
-                    <Divider />
-                    <ListItemButton>
-                        <ListItemText
-                            primary="Default notification ringtone"
-                            secondary="Tethys"
-                        />
-                    </ListItemButton>
-                </List>
+
+                <DialogTitle>Settings</DialogTitle>
+                <DialogContent>
+                    <Box >
+                        <Themes />
+                        <CustomSound />
+                        <SetBGMVolume />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}> <SaveIcon />Save</Button>
+                    <Button onClick={handleClose}> <CloseIcon />Close </Button>
+                </DialogActions>
             </Dialog>
         </React.Fragment>
     );
